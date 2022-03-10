f53fb8b5867c23d9df99766b7c662ad228a31526
Freja <66670040+FHunt-OS@users.noreply.github.com>
2022-03-10 16:58:13 +0000

    remove commeted out code

--- AutoCordon/get_roads.py
+++ AutoCordon/get_roads.py
@@ -1,7 +1,7 @@
 import geopandas as gpd
 from osdatahub import Extent, FeaturesAPI
 
-SRS = 'EPSG:27700'
+CRS = 'EPSG:27700'
 
 
 def get_roads(api_key, polygon):
@@ -13,25 +13,5 @@ def get_roads(api_key, polygon):
         features_api = FeaturesAPI(api_key, product, extent)
         result = features_api.query(10000000)
         roads.extend(result['features'])
-    features_gdf = gpd.GeoDataFrame.from_features(roads, crs=SRS)
+    features_gdf = gpd.GeoDataFrame.from_features(roads, crs=CRS)
     return features_gdf.explode().reset_index(drop=True)
-
-# Choose a Spatial Reference System
-# FEATURE_TYPES = ["Zoomstack_RoadsLocal",
-#                  "Zoomstack_RoadsNational",
-#                  "Zoomstack_RoadsRegional"]
-
-
-# def get_roads(centre_coord, distance, OS_API_KEY):
-#     x, y = centre_coord
-#     bbox = f"{x - distance}, {y - distance}, {x + distance}, {y + distance}"
-#     features = []
-#     for type_name in FEATURE_TYPES:
-#         wfs_api = WFS_API(api_key=OS_API_KEY)
-#         payload = wfs_api.get_all_features_within_bbox(type_name=type_name,
-#                                                        bbox=bbox,
-#                                                        srs=SRS,
-#                                                        allow_premium=True)
-#         features.extend(payload["features"])
-#     features_gdf = gpd.GeoDataFrame.from_features(features, crs=SRS)
-#     return features_gdf.explode().reset_index(drop=True)
